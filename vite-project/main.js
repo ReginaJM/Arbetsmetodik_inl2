1f8bcdf5784f3e5c292d5121fa5ced22d8fb6e31
ReginaJM <reg.johansson@gmail.com>
2024-03-26 19:38:51 +0100

    :sparkles: add nav smooth scroll

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,25 +1,17 @@
-/* import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const linkHiking = document.getElementById("linkHiking");
+  const linkCamping = document.getElementById("linkCamping");
+  
+  const hikingSection = document.getElementById("hikingSection");
+  const campingSection = document.getElementById("campingSection");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
- */
+  linkHiking.addEventListener("click", function() {
+    hikingSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+
+  linkCamping.addEventListener("click", function() {
+    campingSection.scrollIntoView({ behavior: 'smooth', block: 'start'});
+  });
+});
